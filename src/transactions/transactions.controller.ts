c2d1a84718d7aed7e43a42a8cc3495a6fcc430ab
Nam Dang <namquoc.dev@gmail.com>
2025-05-19 14:12:22 +0700

    feat(transaction): unexpose create transtion api

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,20 +1,7 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  UseGuards,
-  Query,
-} from '@nestjs/common';
+import { Controller, Get, Param, UseGuards, Query } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
-import { CreateTransactionDto } from './dto/create-transaction.dto';
-import { UpdateTransactionDto } from './dto/update-transaction.dto';
 import {
   ApiBearerAuth,
-  ApiCreatedResponse,
   ApiOkResponse,
   ApiParam,
   ApiTags,
@@ -37,14 +24,6 @@ import { FindAllTransactionsDto } from './dto/find-all-transactions.dto';
 })
 export class TransactionsController {
   constructor(private readonly transactionsService: TransactionsService) {}
-
-  @Post()
-  @ApiCreatedResponse({
-    type: Transaction,
-  })
-  create(@Body() createTransactionDto: CreateTransactionDto) {
-    return this.transactionsService.create(createTransactionDto);
-  }
 
   @Get()
   @ApiOkResponse({
@@ -81,31 +60,5 @@ export class TransactionsController {
   })
   findById(@Param('id') id: string) {
     return this.transactionsService.findById(id);
-  }
-
-  @Patch(':id')
-  @ApiParam({
-    name: 'id',
-    type: String,
-    required: true,
-  })
-  @ApiOkResponse({
-    type: Transaction,
-  })
-  update(
-    @Param('id') id: string,
-    @Body() updateTransactionDto: UpdateTransactionDto,
-  ) {
-    return this.transactionsService.update(id, updateTransactionDto);
-  }
-
-  @Delete(':id')
-  @ApiParam({
-    name: 'id',
-    type: String,
-    required: true,
-  })
-  remove(@Param('id') id: string) {
-    return this.transactionsService.remove(id);
   }
 }
